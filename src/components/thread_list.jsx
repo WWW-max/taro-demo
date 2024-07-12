aab792fdbaf04e1fcbd089aa44d7eea386a474b2
monster-wu <464711201@qq.com>
2024-07-12 23:26:02 +0800

    feat: v2ex

--- src/components/thread_list.jsx
+++ src/components/thread_list.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { View, Text } from '@tarojs/components'
+import { Thread } from './thread'
+import { Loading } from './loading'
 
-export default function thread_list() {
-  return (
-    <div>thread_list</div>
-  )
+import './thread.css'
+
+class ThreadList extends React.Component {
+  static defaultProps = {
+    threads: [],
+    loading: true,
+  }
+
+  render() {
+    const { loading, threads } = this.props
+
+    if (loading) {
+      return <Loading />
+    }
+
+    const element = threads.map((thread, index) => {
+      return (
+        <Thread
+          key={thread.id}
+          node={thread.node}
+          title={thread.title}
+          last_modified={thread.last_modified}
+          replies={thread.replies}
+          tid={thread.id}
+          member={thread.member}
+        />
+      )
+    })
+
+    return <View className="thread-list">{element}</View>
+  }
 }
+
+export { ThreadList }
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,45 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import Taro from '@tarojs/taro'
+import React from 'react'
+import { View } from '@tarojs/components'
+import { ThreadList } from '../../components/thread_list'
+import api from '../../utils/api'
+
 import './index.css'
 
-export default function Index() {
+class Index extends React.Component {
+  config = {
+    navigationBarTitleText: '首页',
+  }
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+  state = {
+    loading: true,
+    threads: [],
+  }
 
-  return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
-  )
+  async componentDidMount() {
+    try {
+      const res = await Taro.request({
+        url: api.getLatestTopic(),
+      })
+      this.setState({
+        threads: res.data,
+        loading: false,
+      })
+    } catch (error) {
+      Taro.showToast({
+        title: '载入远程数据错误',
+      })
+    }
+  }
+
+  render() {
+    const { loading, threads } = this.state
+    return (
+      <View className="index">
+        <ThreadList threads={threads} loading={loading} />
+      </View>
+    )
+  }
 }
+
+export default Index
